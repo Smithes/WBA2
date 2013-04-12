01a1b0bf1ff030caadf57fb6eb26f479361afeb1
Smithes <steven_buder@gmx.de>
2013-04-12 16:30:30 +0200

    2tes rezept fuer aufgabe 2

--- Phase1/phase 1/neu/Main.java
+++ Phase1/phase 1/neu/Main.java
@@ -1,10 +1,11 @@
+package neu;
+
 import generated.KType;
 import generated.ObjectFactory;
 import generated.RType;
 import generated.RezepteType;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -15,7 +16,6 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 
-package src
 
 
 public class Main {
